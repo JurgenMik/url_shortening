73273fadc6c0fdd19e044d8b3fa0066e57a81043
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-08-16 00:08:11 +0300

    Corrected standard structural errors

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ function App() {
 
   return (
     <div className="w-full h-screen">
-        <div className="flex justify-center mt-20">
-            <div className="grid grid-cols-3 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg h-28 w-1/2">
-                <div className="col-span-2">
-                    <form className="w-full mt-6 ml-6">
-                        <input className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
-                               placeholder="Shorten a link here.."
-                               name="url"
-                        />
-                    </form>
+        <div className="h-28 flex justify-center mt-20">
+            <form className="grid grid-cols-3 w-1/2 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg">
+                <div className="col-span-2 mt-6 ml-6">
+                    <input className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+                           placeholder="Shorten a link here.."
+                           name="url"
+                    />
                 </div>
-                <div className="w-full ml-16 mt-8">
-                    <button type="submit" className="bg-indigo-600 w-1/3 text-white font-bold p-2 rounded-lg">
+                <div className="col-span-1 mt-8 ml-6">
+                    <button className="bg-indigo-600 p-2 rounded-lg text-white font-bold w-1/2 overflow-hidden">
                         Shorten
                     </button>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
   );
